d4717cd6e64765ed73ed84b9bb2ef3fd071e9e90
adraomar <adra.omar2@gmail.com>
2022-08-17 03:54:02 -0300

    Validación de usuario con db y usuario locales

--- src/controlador/ControladorLogin.java
+++ src/controlador/ControladorLogin.java
@@ -1,6 +1,8 @@
 package controlador;
 
+import javax.swing.JOptionPane;
 import modelo.Database;
+import modelo.Usuario;
 import vistas.VistaLogin;
 
 public class ControladorLogin {
@@ -20,5 +22,21 @@ public class ControladorLogin {
     
     public static void botonIniciarSesion() {
         Database db = new Database();
+        String usuario = ventana.getTxtUsuario().getText();
+        String password = ventana.getTxtPassword().getText();
+        Usuario user = db.verificarUsuario(usuario, password);
+        
+        if((user != null) || (usuario.equals("admin") && password.equals("admin")) || (usuario.equals("test") && password.equals("test"))) {
+            JOptionPane.showMessageDialog(null, "Te has conectado correctamente!");
+        } else {
+            JOptionPane.showMessageDialog(null, "El nombre de usuario y/o contraseña que has ingresado es incorrecto.");
+        }
+        
+        limpiar();
+    }
+    
+    public static void limpiar() {
+        ventana.getTxtUsuario().setText("");
+        ventana.getTxtPassword().setText("");
     }
 }
--- src/modelo/Database.java
+++ src/modelo/Database.java
@@ -18,6 +18,27 @@ public class Database {
         }
     }
     
+    public Usuario verificarUsuario(String usuario, String password) {
+        Usuario u = null;
+        PreparedStatement ps;
+        ResultSet rs;
+        String query = "SELECT * FROM accounts WHERE userName = ? AND userPassword = ?";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setString(1, usuario);
+            ps.setString(2, password);
+            rs = ps.executeQuery();
+            
+            if(rs.next()) {
+                u = new Usuario(rs.getInt("id"), rs.getString("lastName"), rs.getString("firstName"), rs.getString("userName"), rs.getString("userPassword"), rs.getInt("rolid"), rs.getString("email"), rs.getString("dni"), true);
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error conexion: " + ex.getMessage());
+        }
+        
+        return u;
+    }
+    
     
     
 }
